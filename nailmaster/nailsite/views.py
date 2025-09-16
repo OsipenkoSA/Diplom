ef99fc6d0bb41e1a79ab536df0155409991712ed
OsipenkoSA <os_s@bk.ru>
2025-09-16 20:25:41 +0300

    16.09.25 html, urls, css, views

--- nailmaster/nailsite/views.py
+++ nailmaster/nailsite/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from .models import ImageWorks
 
 
+def index(request):
+    return render(request, "nailsite/index.html")
+
+
 def nailsite(request):
     wr = ImageWorks.objects.all()
     context = {
         "nailsite": wr
     }
-    return render(request, "nailsite/index.html", context)
+    return render(request, "nailsite/work-gallery.html", context)
